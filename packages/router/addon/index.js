ec9766dbb6a3f240db8960d076d495c3fb44dad5
Stefan Penner <stefan.penner@gmail.com>
2021-06-14 12:57:20 -0600

    Upgrade dependencies (packages/router)

--- packages/router/addon/index.js
+++ packages/router/addon/index.js
@@ -1,9 +1,8 @@
 /*
-  This code is adapted from ember-engines/addon/-private/router-ext.js.
+  This cod- is adapted from ember-engines/addon/-private/router-ext.js.
 */
 import EmberRouter from '@ember/routing/router';
-import { registerWaiter, unregisterWaiter } from '@ember/test';
-import { DEBUG } from '@glimmer/env';
+import { waitForPromise } from '@ember/test-waiters';
 import { macroCondition, getGlobalConfig } from '@embroider/macros';
 
 let Router;
@@ -11,7 +10,7 @@ let Router;
 if (macroCondition(getGlobalConfig()['@embroider/core']?.active)) {
   let newSetup = true;
 
-  const lazyBundle = function lazyBundle(routeName, engineInfoByRoute) {
+  const lazyBundle = function (routeName, engineInfoByRoute) {
     // Here we map engine names to route names. We need to do this because
     // engines can be specified with "as" such as:
     //
@@ -22,22 +21,22 @@ if (macroCondition(getGlobalConfig()['@embroider/core']?.active)) {
     // mappings from routeName to the engines "original name" (which we know at build time).
     let engine = engineInfoByRoute[routeName];
     if (engine && window._embroiderEngineBundles_) {
-      return window._embroiderEngineBundles_.find(bundle => bundle.names.indexOf(engine.name) !== -1);
+      return window._embroiderEngineBundles_.find(
+        (bundle) => bundle.names.indexOf(engine.name) !== -1
+      );
     }
 
     if (window._embroiderRouteBundles_) {
-      return window._embroiderRouteBundles_.find(bundle => bundle.names.indexOf(routeName) !== -1);
+      return window._embroiderRouteBundles_.find(
+        (bundle) => bundle.names.indexOf(routeName) !== -1
+      );
     }
 
     return false;
   };
 
+  // eslint-disable-next-line ember/no-classic-classes
   Router = EmberRouter.extend({
-    init(...args) {
-      this._super(...args);
-      this._inFlightLazyRoutes = 0;
-    },
-
     // This is necessary in order to prevent the premature loading of lazy routes
     // when we are merely trying to render a link-to that points at them.
     // Unfortunately the stock query parameter behavior pulls on routes just to
@@ -61,16 +60,19 @@ if (macroCondition(getGlobalConfig()['@embroider/core']?.active)) {
     // overriding to provide our own handlerResolver.
     setupRouter() {
       let isSetup = this._super(...arguments);
+
+      // eslint-disable-next-line ember/no-private-routing-service
+      const routerMicrolib = this._routerMicrolib;
       if (newSetup) {
         // Different versions of routerMicrolib use the names `getRoute` vs
         // `getHandler`.
-        if (this._routerMicrolib.getRoute !== undefined) {
-          this._routerMicrolib.getRoute = this._handlerResolver(
-            this._routerMicrolib.getRoute.bind(this._routerMicrolib)
+        if (routerMicrolib.getRoute !== undefined) {
+          routerMicrolib.getRoute = this._handlerResolver(
+            routerMicrolib.getRoute.bind(routerMicrolib)
           );
-        } else if (this._routerMicrolib.getHandler !== undefined) {
-          this._routerMicrolib.getHandler = this._handlerResolver(
-            this._routerMicrolib.getHandler.bind(this._routerMicrolib)
+        } else if (routerMicrolib.getHandler !== undefined) {
+          routerMicrolib.getHandler = this._handlerResolver(
+            routerMicrolib.getHandler.bind(routerMicrolib)
           );
         }
       }
@@ -78,39 +80,22 @@ if (macroCondition(getGlobalConfig()['@embroider/core']?.active)) {
     },
 
     _handlerResolver(original) {
-      return name => {
+      return (name) => {
         let bundle = lazyBundle(name, this._engineInfoByRoute);
         if (!bundle || bundle.loaded) {
           return original(name);
         }
-        this._inFlightLazyRoutes++;
-        return bundle.load().then(
-          () => {
-            this._inFlightLazyRoutes--;
+        return waitForPromise(
+          (async () => {
+            await bundle.load();
             bundle.loaded = true;
             return original(name);
-          },
-          err => {
-            this._inFlightLazyRoutes--;
-            throw err;
-          }
+          })(),
+          'embroider:lazy-routes'
         );
       };
     },
   });
-
-  if (DEBUG) {
-    Router.reopen({
-      init(...args) {
-        this._super(...args);
-        this._doneLoadingLazyRoutes = () => this._inFlightLazyRoutes < 1;
-        registerWaiter(this._doneLoadingLazyRoutes);
-      },
-      willDestroy() {
-        unregisterWaiter(this._doneLoadingLazyRoutes);
-      },
-    });
-  }
 } else {
   Router = EmberRouter;
 }
